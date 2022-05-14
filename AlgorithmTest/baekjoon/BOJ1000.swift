d0f5c530f3671735d0c6963f54180cfa2511c63e
jsh9611 <jangsh9611@naver.com>
2022-05-14 14:26:14 +0900

    Solve BOJ1000

---
+++ AlgorithmTest/baekjoon/BOJ1000.swift
@@ -0,0 +1,14 @@
+//
+//  BOJ_1000.swift
+//  AlgorithmTest
+//
+//  Created by SeongHoon Jang on 2022/05/14.
+//
+
+import Foundation
+
+let line = readLine()!
+let lineArr = line.components(separatedBy: " ")
+let a = Int(lineArr[0])!
+let b = Int(lineArr[1])!
+print(a+b)
