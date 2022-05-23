45ea5e47c1fb177a74d597f0f71913db2a98b95c
jsh9611 <jangsh9611@naver.com>
2022-05-23 13:57:22 +0900

    Solve Programmers_가장큰정사각형찾기.swift

---
+++ AlgorithmTest/programmers/가장큰정사각형찾기.swift
@@ -0,0 +1,43 @@
+//
+//  가장큰정사각형찾기.swift
+//  AlgorithmTest
+//
+//  Created by SeongHoon Jang on 2022/05/23.
+//
+
+import Foundation
+
+func solution(_ board:[[Int]]) -> Int {
+    let x = board.count
+    let y = board[0].count
+    var answer:Int = 0
+    var copy = board.map{$0.map{$0}};
+    
+    for i in 0..<x {
+        if board[i][0] == 1 {
+            answer = 1
+            break
+        }
+    }
+    for j in 0..<y {
+        if board[0][j] == 1 {
+            answer = 1
+            break
+        }
+    }
+    for i in 1..<x {
+        for j in 1..<y {
+            if board[i][j] == 0 {
+                continue
+            }
+            copy[i][j] = min(copy[i-1][j-1], copy[i-1][j], copy[i][j-1]) + 1
+            answer = max(answer, copy[i][j])
+        }
+    }
+    
+    return answer * answer
+}
+
+print(solution([[0,0,1,1],[1,1,1,1]]))  // 4
+print(solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]))  // 9
+print(solution([[0, 0, 0, 0],[1, 0, 0, 0],[1, 0, 0, 0],[0, 0, 0, 0]])) // 1
