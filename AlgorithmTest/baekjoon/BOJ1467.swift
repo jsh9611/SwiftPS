b67ff688b058029a38d33f81e92cceaa9c5a4883
jsh9611 <jangsh9611@naver.com>
2022-08-16 12:56:00 +0900

    Solve BOJ1467_DP

--- AlgorithmTest/baekjoon/BOJ1467.swift
+++ AlgorithmTest/baekjoon/BOJ1467.swift
@@ -1,8 +1,45 @@
 //
 //  BOJ1467.swift
-//  AlgorithmTest
-//
-//  Created by SeongHoon Jang on 2022/08/05.
+//  https://www.acmicpc.net/problem/1463
+//  1로 만들기
 //
 
 import Foundation
+
+let num = Int(readLine()!)!
+
+print(answer(n: num))
+
+func answer(n: Int) -> Int {
+    if n == 1 {
+        return 0
+    }
+    
+    if n < 4 {
+        return 1
+    }
+
+    var dp = [Int](repeating: 0, count: num+1)
+    
+    dp[1] = 0
+    dp[2] = 1
+    dp[3] = 1
+    
+    for i in 4...n {
+        
+        var temp2 = 99999
+        var temp3 = 99999
+        
+        if i % 2 == 0 {
+            temp2 = dp[i/2] + 1
+        }
+        
+        if i % 3 == 0 {
+            temp3 = dp[i/3] + 1
+        }
+        
+        dp[i] = min(dp[i-1]+1, temp2, temp3)
+    }
+    
+    return dp[n]
+}
