1a66326d79812db47570103b9836d5e4ae703d8a
SeongHoon Jang <57349859+jsh9611@users.noreply.github.com>
2022-12-31 17:35:17 +0900

    [Silver IV] Title: 스택, Time: 28 ms, Memory: 69500 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.swift
+++ 백준/Silver/10828. 스택/스택.swift
@@ -1,16 +1,12 @@
 var arr: [String] = []
 for _ in 0..<Int(readLine()!)! {
     let cmd = readLine()!.split{ $0 == " " }.map{ String($0) }
+    
     switch cmd[0] {
-    case "push":
-        arr.append(cmd[1])
-    case "pop":
-        print(arr.isEmpty ? -1 : arr.removeLast())
-    case "size":
-        print(arr.count)
-    case "empty":
-        print(arr.isEmpty ? 1 : 0)
-    default:
-        print(arr.isEmpty ? -1 : arr.last!)
+    case "push": arr.append(cmd[1])
+    case "pop": print(arr.isEmpty ? -1 : arr.removeLast())
+    case "size": print(arr.count)
+    case "empty": print(arr.isEmpty ? 1 : 0)
+    default: print(arr.isEmpty ? -1 : arr.last!)
     }
 }
