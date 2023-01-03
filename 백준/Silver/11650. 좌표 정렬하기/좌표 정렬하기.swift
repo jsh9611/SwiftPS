1bb1c062da70490a9643a208ec2f465bb51d003a
SeongHoon Jang <57349859+jsh9611@users.noreply.github.com>
2023-01-03 20:02:42 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 188 ms, Memory: 73764 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.swift
@@ -0,0 +1,26 @@
+let n = Int(readLine()!)!
+var tupples = [(Int, Int)]()
+for _ in 0..<n {
+    let input = readLine()!.split{ $0 == " " }.map{ Int($0)! }
+    tupples.append((input[0], input[1]))
+}
+
+let customSort: ((Int,Int), (Int,Int)) -> Bool = { a, b in
+    if a.0 != b.0 {
+        return a < b
+    } else {
+        if a.1 != b.1 {
+            return a < b
+        }
+    }
+    return false
+}
+
+tupples.sort(by: customSort)
+
+var answer = ""
+for tupple in tupples {
+    answer += "\(tupple.0) \(tupple.1)\n"
+}
+
+print(answer)
