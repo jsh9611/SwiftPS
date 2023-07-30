019978ff949d1a2759192b53886312448bb97531
SeongHoon Jang <57349859+jsh9611@users.noreply.github.com>
2023-07-30 09:39:52 +0900

    [Silver II] Title: 병사 배치하기, Time: 12 ms, Memory: 69260 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.swift
@@ -0,0 +1,31 @@
+let n = Int(readLine()!)!
+
+var arr = readLine()!.split{ $0 == " " }.map{ Int($0)! }
+var dp = [Int]()
+
+func lowerBound(_ val: Int) -> Int {
+    var first = 0
+    var last = dp.count - 1
+    
+    while first < last {
+        let mid = (first + last) / 2
+        if dp[mid] <= val {
+            last = mid
+        } else {
+            first = mid + 1
+        }
+    }
+    
+    return last
+}
+
+for i in 0..<n {
+    let last = lowerBound(arr[i])
+    if last == -1 || dp[last] > arr[i] {
+        dp.append(arr[i])
+    } else {
+        dp[last] = arr[i]
+    }
+}
+
+print(n - dp.count)
