456f3936fb9ad81953658eb6ae7fca6fb5c15d7b
SeongHoon Jang <57349859+jsh9611@users.noreply.github.com>
2023-08-06 10:17:39 +0900

    [Silver II] Title: N번째 큰 수, Time: 524 ms, Memory: 149928 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+// 라이노님 입출력 코드
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+}
+
+let fIO = FileIO()
+
+let n = fIO.readInt()
+var arr = [Int]()
+
+for _ in 0..<n*n {
+    arr.append(fIO.readInt())
+}
+
+arr.sort(by: >)
+
+print(arr[n-1])
