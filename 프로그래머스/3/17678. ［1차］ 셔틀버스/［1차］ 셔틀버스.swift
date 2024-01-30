aa9d3aef6d1f4fcb9e32ea4d17c0fed0a34275d3
Antony <57349859+jsh9611@users.noreply.github.com>
2024-01-30 19:33:18 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 7.33 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.swift
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.swift
@@ -1,46 +1,24 @@
 func solution(_ n:Int, _ t:Int, _ m:Int, _ timetable:[String]) -> String {
-    var times = [Int]()
-    for time in timetable {
-        let temp = time.split(separator: ":").map{ Int($0)! }
-        let minutes = temp[0]*60 + temp[1]
-        times.append(minutes)
-    }
+    let startTime = 9*60
+    let sortedTimetable = timetable.map { Int($0.prefix(2))!*60 + Int($0.suffix(2))! }.sorted()
+    let crewCount = timetable.count
     
-    times.sort(by: >)
-
-    var currentTime = 9*60
-    var crew = 60*24
-    var M = 0
+    var crewIndex = 0
+    var lastTime = startTime + (n-1) * t
     
-
-    for _ in 0..<n-1 {
-        M = m
-        while M > 0 && !times.isEmpty && times.last! <= currentTime {
-            _ = times.popLast()!
-            M -= 1
-        }
-        currentTime += t
-    }
-    
-    M = m
-    while !times.isEmpty {
-        if M == 1 {
-            if !times.isEmpty {
-                let last = times.last!
-                if last <= currentTime {
-                    currentTime = last - 1
+    for idx in 0..<n {
+        var cnt = 0
+        while crewIndex < crewCount, sortedTimetable[crewIndex] <= startTime + (idx * t) {
+            cnt += 1
+            crewIndex += 1
+            if cnt == m {
+                if idx == n-1 {
+                    lastTime = sortedTimetable[crewIndex-1]-1
                 }
-            } else {
-                if crew <= currentTime {
-                    currentTime = crew - 1
-                }
+                break
             }
-            break
         }
-        
-        crew = times.popLast()!
-        M -= 1
     }
     
-    return "\(currentTime/60 < 10 ? "0" : "")\(currentTime/60):\(currentTime%60 < 10 ? "0" : "")\(currentTime%60)"
+    return String(format: "%02d:%02d", lastTime/60, lastTime%60)
 }
